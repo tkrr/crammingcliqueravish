7197be226e8b968944da8b44ca903773f2b61a0d
Ravish Rao <Adkaravish_Rao@ad.infosys.com>
2018-01-13 13:55:12 -0600

    temp

--- assets/javascript/profile.js
+++ assets/javascript/profile.js
@@ -1,4 +1,4 @@
-if (!checkIfLoggedIn()) {
+if (sessionStorage.getItem("userSessionEntity") === null) {
     window.location.href = "index.html";
 }
 
@@ -9,7 +9,7 @@ $(function() {
     console.log("email from session: " + userSessionEntity.email);
     //get connection to database
     // Initialize Firebase
-    /*var firebaseConfig = {
+    var firebaseConfig = {
         apiKey: "AIzaSyCxL4l6YBouk-C92wcTeZ_sZbzQDcR00hE",
         authDomain: "crammingclique.firebaseapp.com",
         databaseURL: "https://crammingclique.firebaseio.com",
@@ -20,7 +20,7 @@ $(function() {
     firebase.initializeApp(firebaseConfig);
     var database = firebase.database();
     console.log("connected to firebase");
-	*/
+	
     //get the user details stored in database
     database.ref("/crammingUsers").orderByChild("email").equalTo(userSessionEntity.email).once("value", function(snapshot) {
         console.log("Read database");
