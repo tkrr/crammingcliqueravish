c57084aec768b87b31a8b215dd4c4c76350664ee
Ravish Rao <Adkaravish_Rao@ad.infosys.com>
2018-01-13 13:46:15 -0600

    temp

--- assets/javascript/profile.js
+++ assets/javascript/profile.js
@@ -28,7 +28,7 @@ $(function() {
             console.log("Error!! record not found: " + snapshot);
             return;
         } else {
-            console.log("user found");
+            console.log("user found" + snapshot.val());
             //prepopulate the profile fields from data pulled from the table                   
         	$("#validationName").text(snapshot.val().name);
         	$("#userImg").attr("src",snapshot.val().imageUrl);
