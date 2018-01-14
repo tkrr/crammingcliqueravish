796f78de3cdb99f6d9f268ea229b3793089d69ce
Ravish Rao <Adkaravish_Rao@ad.infosys.com>
2018-01-13 19:14:05 -0600

    temp

--- assets/javascript/dbutil.js
+++ assets/javascript/dbutil.js
@@ -1,28 +1,13 @@
-function Database() {
-    this.firebaseConfig = {
-            apiKey: "AIzaSyCxL4l6YBouk-C92wcTeZ_sZbzQDcR00hE",
-            authDomain: "crammingclique.firebaseapp.com",
-            databaseURL: "https://crammingclique.firebaseio.com",
-            projectId: "crammingclique",
-            storageBucket: "crammingclique.appspot.com",
-            messagingSenderId: "321767599885"
-        },
-        this.firebaseDB = null,
-        this.initialize = function() {
-            firebase.initializeApp(this.firebaseConfig);
-            this.firebaseDB = firebase.database();
-            console.log("connected to firebase");
-        },
-        this.getUserByEmail = function(email) {
-            this.firebaseDB.ref("/crammingUsers").orderByChild("email").equalTo(email).once("child_added", function(snapshot) {
-                console.log(snapshot.val());
-                if (snapshot.val() === null) {
-                    console.log("Error!! record not found: ");
-                    return null;
-                } else {
-                    console.log("user found" + snapshot.val().name);
-                    return snapshot.val();
-                }
-            });
-        }
-}
+firebaseConfig = {
+    apiKey: "AIzaSyCxL4l6YBouk-C92wcTeZ_sZbzQDcR00hE",
+    authDomain: "crammingclique.firebaseapp.com",
+    databaseURL: "https://crammingclique.firebaseio.com",
+    projectId: "crammingclique",
+    storageBucket: "crammingclique.appspot.com",
+    messagingSenderId: "321767599885"
+};
+firebase.initializeApp(firebaseConfig);
+
+
+// Create a variable to reference the database
+var database = firebase.database();
--- assets/javascript/profile.js
+++ assets/javascript/profile.js
@@ -1,35 +1,62 @@
-
-
 $(function() {
-	var database = new Database();
-	database.initialize();
+    var database = new Database();
+    database.initialize();
 
-	if (sessionStorage.getItem("userSessionEntity") === null) {
-    	window.location.href = "index.html";
-    	return;
-	}
+    if (sessionStorage.getItem("userSessionEntity") === null) {
+        window.location.href = "index.html";
+        return;
+    }
 
     //get the current user from session 
     var userSessionEntity = JSON.parse(sessionStorage.getItem("userSessionEntity"));
     console.log("email from session: " + userSessionEntity.email);
     //get connection to database
-	
-    //get the user details stored in database
-    var ccUser = database.getUserByEmail("userSessionEntity.email");
-    console.log("DB response received:" + ccUser);
-            //prepopulate the profile fields from data pulled from the table                   
-        	//$("#validationName").val(snapshot.val().name);
-        	//$("#userImg").attr("src",snapshot.val().imageUrl);
-        	//$("#phoneNum").val(snapshot.val().phone);
-        	//$("#customCheck1").attr("src",snapshot.val().imageUrl);
 
-    
+    //get the user details stored in database
+    database.ref("/crammingUsers").orderByChild("email").equalTo(userSessionEntity.email).once("child_added", function(snapshot) {
+        console.log(snapshot.val());
+        if (snapshot.val() === null) {
+            console.log("Error!! record not found: " + snapshot);
+            return;
+        } else {
+            console.log("user found" + snapshot.val().name);
+            //prepopulate the profile fields from data pulled from the table                   
+            $("#validationName").val(snapshot.val().name);
+            $("#userImg").attr("src", snapshot.val().imageUrl);
+            $("#phoneNum").val(snapshot.val().phone);
+            //$("#customCheck1").attr("src",snapshot.val().imageUrl);
+
+        }
+
+    });
+
+
     //save button clink event
     $(document).on("click", "#saveProfileButton", function(event) {
         event.preventDefault();
         //validate the data
         //if error throw error back
         //update the database with details
+        database.ref("/crammingUsers").orderByChild("email").equalTo(userSessionEntity.email).once("child_added", function(snapshot) {
+        console.log(snapshot.val());
+        if (snapshot.val() === null) {
+            console.log("Error!! record not found: " + snapshot);
+                    var crammingUser = {
+                        "id": userSessionEntity.id,
+                        "name": $("#validationName").val(),
+                        "imageUrl": userSessionEntity.imageUrl,
+                        "email": userSessionEntity.email,
+                        "phone": $("#phoneNum").val(),
+                        "receiveTextNotification": true
+                    };
+                    database.ref("/crammingUsers").push(crammingUser);
+        } else {
+            console.log("user found" + snapshot.val().name);
+            snapshot.ref.update({ name: $("#validationName").val()  });
+        }
+
+    });
+
         //redirect the feed page	        
     });
 
