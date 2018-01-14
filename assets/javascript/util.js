58d5fd9ec664b8599c696aaa982ce1d2e4effbf3
Ravish Rao <Adkaravish_Rao@ad.infosys.com>
2018-01-14 08:01:37 -0600

    temp

--- assets/javascript/util.js
+++ assets/javascript/util.js
@@ -10,17 +10,3 @@ var page = path.split("/").pop();
 });
 console.log("after redirect check");
 
-firebaseConfig = {
-    apiKey: "AIzaSyCxL4l6YBouk-C92wcTeZ_sZbzQDcR00hE",
-    authDomain: "crammingclique.firebaseapp.com",
-    databaseURL: "https://crammingclique.firebaseio.com",
-    projectId: "crammingclique",
-    storageBucket: "crammingclique.appspot.com",
-    messagingSenderId: "321767599885"
-};
-firebase.initializeApp(firebaseConfig);
-
-
-// Create a variable to reference the database
-var database = firebase.database();
-console.log("DB loading done");
