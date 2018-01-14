aaf4d36f936546c6d490f95abe49b1f09a4af9d3
Ravish Rao <Adkaravish_Rao@ad.infosys.com>
2018-01-13 18:47:40 -0600

    temp

--- assets/javascript/dbutil.js
+++ assets/javascript/dbutil.js
@@ -6,24 +6,24 @@ function Database() {
             projectId: "crammingclique",
             storageBucket: "crammingclique.appspot.com",
             messagingSenderId: "321767599885"
-    },
-    this.firebaseDB = null,
-    this.initialize = function() {
-        firebase.initializeApp(firebaseConfig);
-        this.firebaseDB = firebase.database();
-        console.log("connected to firebase");
-    },
-    this.getUserByEmail = function(email) {
-        this.database.ref("/crammingUsers").orderByChild("email").equalTo(email).once("child_added", function(snapshot) {
-            new Promise(resolve => setTimeout(resolve, 10000));
-            console.log(snapshot.val());
-            if (snapshot.val() === null) {
-                console.log("Error!! record not found: ");
-                return null;
-            } else {
-                console.log("user found" + snapshot.val().name);
-                return snapshot.val();
-            }
+        },
+        this.firebaseDB = null,
+        this.initialize = function() {
+            firebase.initializeApp(firebaseConfig);
+            this.firebaseDB = firebase.database();
+            console.log("connected to firebase");
+        },
+        this.getUserByEmail = function(email) {
+            this.database.ref("/crammingUsers").orderByChild("email").equalTo(email).once("child_added", function(snapshot) {
+                new Promise(resolve => setTimeout(resolve, 10000));
+                console.log(snapshot.val());
+                if (snapshot.val() === null) {
+                    console.log("Error!! record not found: ");
+                    return null;
+                } else {
+                    console.log("user found" + snapshot.val().name);
+                    return snapshot.val();
+                }
+            });
         }
-    }
 }
