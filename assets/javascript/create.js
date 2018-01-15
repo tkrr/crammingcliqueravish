87f357f93ce582c011def77e322fe86d2bd5d7fc
Ravish Rao <Adkaravish_Rao@ad.infosys.com>
2018-01-15 17:38:08 -0600

    temp

--- assets/javascript/create.js
+++ assets/javascript/create.js
@@ -1,16 +1,33 @@
-if (!checkIfLoggedIn()) {
-    window.location.href = "index.html";
-}
-
-
 $(function() {
-//get the current user from session 
-	//if user not present, then redirect to index.html
+    //get the current user from session 
+    //if user not present, then redirect to index.html
 
-//save button clink event
-	//validate the data
-		//if error throw error back
-	//update the database with details
-		//redirect the feed page		
+    //save button clink event
+    $(document).on("click", "#btnCreateEvent", function(event) {
+        event.preventDefault();
+        //validate the data
+        //if error throw error back
+        //update the database with details
+        var userSessionEntity = JSON.parse(sessionStorage.getItem("userSessionEntity"));
 
+        console.log("on click event");
+
+        var crammingUser = {
+            "id": userSessionEntity.id,
+            "name": $("#validationName").val(),
+        };
+        database.ref("/crammingUsers").push(crammingUser);
+        //redirect the feed page
+        window.location.href = "feed.html";
+    });
+
+    function initAutocomplete() {
+        // Create the autocomplete object, restricting the search to geographical
+        // location types.
+        var autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */
+            (document.getElementById('autocomplete')));
+
+    };
+    initAutocomplete();
 });
