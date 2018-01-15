7631ded010fb7ee48d39e26acdc72c4402a87e89
Ravish Rao <Adkaravish_Rao@ad.infosys.com>
2018-01-15 00:23:21 -0600

    temp

--- assets/javascript/util.js
+++ assets/javascript/util.js
@@ -1,12 +1,11 @@
 var path = window.location.pathname;
 var page = path.split("/").pop();
-
+console.log(page);
 (function() {
     if (page !== "index.html" && sessionStorage.getItem("userSessionEntity") === null) {
+        console.log("redirecting");
         window.location.href = "index.html";
         return;
-        console.log("after redirect");
+
     }
 });
-console.log("after redirect check");
-
